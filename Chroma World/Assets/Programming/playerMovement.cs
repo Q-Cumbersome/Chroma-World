b19b6ebf096eed8a3b7d747f0cf36b596c170831
Q-Cumbersome <carterquatt@gmail.com>
2023-03-30 16:22:00 -0400

    Movement fully functional, jump in progress

--- Chroma World/Assets/Programming/playerMovement.cs
+++ Chroma World/Assets/Programming/playerMovement.cs
@@ -4,26 +4,60 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private float moveSpeed = 10f;
-    private Vector2 movement;
+    public float moveSpeed = 200;
+    public float jumpForce = 10;
 
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private float movementX;
+    private float movementY;
+    private bool onGround = false;
+
+    //private int dirHeld = -1;
+
+    private Vector2[] directions = new Vector2[] {
+        Vector2.right, Vector2.left
+    };
+
+    private KeyCode[] keys = new KeyCode[] {
+      KeyCode.RightArrow, KeyCode.LeftArrow,
+      KeyCode.D,          KeyCode.A 
+    };
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D> ();
     }
+
     
     // Listens for player input
     void FixedUpdate(){
-            movement.x = Input.GetAxisRaw ("Horizontal");
-            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //Vector2 v = rb.velocity;
+        //v.y = jumpForce * moveSpeed;
+        //rb.velocity = v;
+
+        rb.velocity = new Vector2(movement.x * moveSpeed * Time.deltaTime, rb.velocity.y);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // May be useful for changing direction of sprites
+        /* dirHeld = -1;
+        for (int i=0; i <keys.Length; i++) {
+            if ( Input.GetKey(keys[i]) ) {
+                movementX = Input.GetAxisRaw ("Horizontal");
+                dirHeld = i % 2;
+            }
+        } */
+
+        movementX = Input.GetAxisRaw ("Horizontal");
+        movementY = Input.GetAxisRaw ("Vertical");
+
+        movement = new Vector2(movementX, movementY);
+
+
         
     }
 }
